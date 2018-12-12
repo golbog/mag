4b708135943ab8b38fcd3f8b7f6e2a9a2d34bbf1
Unknown <bogdan.golobic@gmail.com>
2018-12-12 14:50:08 +0100

    Minor fixes to utils

--- code/cheauder_utils.py
+++ code/cheauder_utils.py
@@ -7,6 +7,8 @@ import numpy as np
 import argparse
 from collections import OrderedDict
 from keras.models import Model, model_from_json
+from sklearn.manifold import TSNE, MDS
+from sklearn.decomposition import PCA
 
 
 
@@ -14,8 +16,8 @@ import csv
 import json
 import numpy as np
 
-def load_data(filename, charset_filename=None, col_smiles=0, col_target=1, start_row=1, delimiter=' ', quotechar='\"', max_len=-1):
-    X, y = read_data(filename, col_smiles, col_target, start_row, delimiter, quotechar)
+def load_data_vae(filename, charset_filename=None, col_smiles=0, col_target=1, start_row=1, delimiter=' ', quotechar='\"', max_len=-1):
+    X, y, label = read_data(filename, col_smiles, col_target, start_row, delimiter, quotechar)
     if charset_filename is None:
         chars, charset = create_charset(X)
     else:
@@ -25,16 +27,27 @@ def load_data(filename, charset_filename=None, col_smiles=0, col_target=1, start
     X = vectorize_smiles(X, charset, max_len)
     return X, y, charset, chars
 
-def load_fingerprints(filename, col_smiles=0, col_target=1, start_row=1, delimiter=' ', quotechar='\''):
-    smiles, y = read_data('../data/BBBP.csv', col_smiles=3, col_target=2, delimiter=',')
+def load_data(filename, encoder, charset_filename=None, col_smiles=0, col_target=1, start_row=1, delimiter=' ', quotechar='\"', max_len=-1):
+    smiles, y, label = read_data(filename, col_smiles=3, col_target=2, delimiter=',')
+
+    # VAE
+    if charset_filename is None:
+        chars, charset = create_charset(X)
+    else:
+        chars, charset = load_charset(charset_filename)
+    if max_len == -1:
+        max_len = max([len(x) for x in X]) + 1
+    Xvae, _ , _ = encoder.predict(vectorize_smiles(smiles, charset, max_len))
+
+    # fingerprints
     moles = np.array([AllChem.MolFromSmiles(x) for x in smiles])
 
     valid = [i for i, x in enumerate(moles) if x is not None]
     moles = moles[valid]
     y = np.array(y[valid], dtype=np.float)
+    Xvae = Xvae[valid] # also remove from encoded
 
     fingerprints = [AllChem.GetMorganFingerprintAsBitVect(x, 4) for x in moles]
-
     Xfinger = list()
     for x in fingerprints:
         arr = np.zeros((0,))
@@ -42,7 +55,27 @@ def load_fingerprints(filename, col_smiles=0, col_target=1, start_row=1, delimit
         Xfinger.append(arr)
     Xfinger = np.array(Xfinger)
 
-    return Xfinger, y, valid
+    return Xvae, Xfinger, y, label, smiles
+
+
+
+def load_fingerprints(filename, col_smiles=0, col_target=1, start_row=1, delimiter=' ', quotechar='\''):
+    smiles, y, label = read_data(filename, col_smiles=3, col_target=2, delimiter=',')
+    moles = np.array([AllChem.MolFromSmiles(x) for x in smiles])
+
+    valid = [i for i, x in enumerate(moles) if x is not None]
+    moles = moles[valid]
+    y = np.array(y[valid], dtype=np.float)
+
+    fingerprints = [AllChem.GetMorganFingerprintAsBitVect(x, 4) for x in moles]
+    Xfinger = list()
+    for x in fingerprints:
+        arr = np.zeros((0,))
+        DataStructs.ConvertToNumpyArray(x, arr)
+        Xfinger.append(arr)
+    Xfinger = np.array(Xfinger)
+
+    return Xfinger, y, valid, label
 
 def is_valid_smiles(a):
     return AllChem.MolFromSmiles(a) is not None and a != '' and a is not None
@@ -52,17 +85,19 @@ def read_data(filename, col_smiles=0, col_target=1, start_row=1, delimiter=' ', 
     targets = list()
     with open(filename) as file:
         reader = csv.reader(file, delimiter=delimiter, quotechar=quotechar)
-        for _ in range(start_row): #testiraj
-            next(reader)
+        for i in range(start_row): #testiraj
+            row = next(reader)
+            if i == 0:
+                label = row[col_target]
         for row in reader:
             if len(row) > 2:
                 row = np.array(row)
                 smile = row[col_smiles]
-                if smile[-1] != '\n':
-                    smile += '\n'
+                if smile[-1] == '\n':
+                    smile = smile[:-1]
                 smiles.append(smile)
                 targets.append(row[col_target] if col_target != -1 else 0)
-    return np.array(smiles), np.array(targets)
+    return np.array(smiles), np.array(targets), label
 
 def load_charset(charset_filename):
     with open(charset_filename, "r") as f:
@@ -171,6 +206,9 @@ def correctly_decoded_with_tries(X, X_real, decoder, chars, noise_norm=False, n=
 def classification_test(Xs, y, classifiers, n_splits=10):
     res = OrderedDict()
     cv = StratifiedKFold(n_splits=n_splits)
+
+    Xs['Joined'] = np.vstack((list(Xs.values())))
+
     for Xname, X in Xs.items():
         for cname, c in classifiers.items():
             val = 0
@@ -178,7 +216,7 @@ def classification_test(Xs, y, classifiers, n_splits=10):
                 c.fit(X[train], y[train])
                 val += roc_auc_score(y[test], c.predict(X[test]))
                 res[(Xname, cname)] = val/n_splits
-            print("{} | {} : {}".format(Xname, cname, val / n_splits))
+            print("{:10s} | {:10s} : {:.2f}".format(Xname, cname, val / n_splits))
     return res
 
 def perturb_z(z, noise_norm=False):
@@ -187,6 +225,31 @@ def perturb_z(z, noise_norm=False):
         noise_vec = noise_vec / np.linalg.norm(noise_vec)
         return z + (noise_norm * noise_vec)
     return z
+
+def to_csv(filename, smiles, X, y):
+    with open(filename, 'w') as f:
+        line = '{},{},{}\n'.format('smiles', ','.join('dim{}'.format(i) for i in range(len(X))), 'y')
+        f.write(line)
+        for i in range(len(X)):
+            line = '{},{},{}\n'.format(smiles[i], ','.join('{}'.format(x) for x in X[i]), y[i])
+            f.write(line)
+        return True
+
+def plot_tsne_classification(X, y, title, legend_title):
+    pca = PCA(n_components=20)
+    pca_res = pca.fit_transform(X)
+
+    tsne = TSNE(n_components=2, verbose=1, perplexity=30, n_iter=500)
+    res = tsne.fit_transform(pca_res)
+
+    for yi in np.unique(y):
+        plt.plot(np.array(res[:, 0])[y == yi], np.array(res[:, 1])[y == yi], 'o', label=str(bool(yi)), alpha=.3,
+                 markersize=4)
+    plt.title(title)
+    plt.xlabel('TSNE 1. component')
+    plt.ylabel('TSNE 2. component')
+    plt.legend(title=legend_title, loc='top right')
+    plt.show()
 
 if __name__ == '__main__':
     X,y,charset,chars = load_data('../data/250k_rndm_zinc_drugs_clean_3small.csv', col_smiles=0, col_target=1, delimiter=',', max_len=120)
